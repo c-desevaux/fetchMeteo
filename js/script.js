dbe0e3acd9cc74d09ffac421ddff6178a9a2c2ce
cdesevaux <corentin.desevaux@gmail.com>
2025-10-30 14:41:32 +0100

    animation css

--- js/script.js
+++ js/script.js
@@ -60,13 +60,13 @@ console.log(data);
         // Partie à renseigner à l'aide de la documentation technique en ligne et de votre formateur
         data__city.textContent = data.location.name;                                                     // Mettre à jour data__city.textContent
         data__humidity.innerHTML = '<i class="fas fa-droplet"></i> ' +data.current.humidity+"%";         // Mettre à jour data__humidity.innerHTML 
-        data__updated.textContent =   data.current.last_updated                                          // Mettre à jour data__updated.textContent 
+        data__updated.textContent =  "Mis à jour à: "+ data.current.last_updated.slice(11);                                        // Mettre à jour data__updated.textContent 
         data__wind.innerHTML = '<i class="fas fa-wind"></i> '+data.current.wind_kph+"km/h "+direction[data.current.wind_dir];               // Mettre à jour data__wind.innerHTML
         data__temperature.textContent = data.current.temp_c+"°C";                                          // Mettre à jour data__temperature.textContent 
         
         
         // Mettre à jour card.classList
-
+        card.classList.add("appearCard");
         // Mettre à jour  l'url de l'icone météo reçue data__icon.src 
 
         data__icon.src = data.current.condition.icon;
